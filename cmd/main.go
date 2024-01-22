1755b07aebb14a9d3e07c959024b100cadf98fe7
Matano <mwakimamatano@gmail.com>
2024-01-22 21:04:32 +0300

    Show Triangles

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"runtime"
+	"strings"
 
 	"github.com/go-gl/gl/v4.6-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
@@ -24,6 +26,57 @@ func processInput(window *glfw.Window) {
 	}
 }
 
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+	sourceInC, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, sourceInC, nil)
+	free()
+	gl.CompileShader(shader)
+	var hasSuccessfullyCompiled int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &hasSuccessfullyCompiled)
+	if hasSuccessfullyCompiled == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+	return shader, nil
+}
+
+func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
+	program := gl.CreateProgram()
+
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+
+	var hasSuccessfullyCompiled int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &hasSuccessfullyCompiled)
+	if hasSuccessfullyCompiled == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+		return 0, fmt.Errorf("failed to link program: %v", log)
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	return program, nil
+}
+
 func main() {
 	err := glfw.Init()
 	if err != nil {
@@ -34,7 +87,7 @@ func main() {
 	glfw.WindowHint(glfw.ContextVersionMinor, 3)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 
-	window, err := glfw.CreateWindow(800, 600, "Testing", nil, nil)
+	window, err := glfw.CreateWindow(800, 600, "Learn Open GL", nil, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -45,7 +98,60 @@ func main() {
 		log.Fatalln("Failed to start gl", err)
 	}
 	gl.Viewport(0, 0, 800, 600)
-	glfw.GetCurrentContext().SetFramebufferSizeCallback(framebufferSizeCallback)
+	window.SetFramebufferSizeCallback(framebufferSizeCallback)
+
+	// build and compile our shader program
+	shaderEndChar := "\x00"
+	// vertices
+	triangleVertices := []float32{
+		// x, y, z
+		-0.5, -0.5, 0, // left
+		0.5, -0.5, 0, // right
+		0, 0.5, 0, // top
+
+	}
+
+	// configure the vertex data
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, len(triangleVertices)*4, gl.Ptr(triangleVertices), gl.STATIC_DRAW)
+
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, gl.Ptr(nil))
+	gl.EnableVertexAttribArray(0)
+
+	// vertexShader
+
+	vertexShaderSource := `
+  #version 330 core
+
+  layout (location = 0) in vec3 aPos;
+
+  void main() {
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); 
+  }
+` + shaderEndChar
+	compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+
+	// fragmentShader
+
+	fragmentShaderSource := `
+  #version 330 core
+  out vec4 FragColor;
+  void main() {
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+  }
+` + shaderEndChar
+
+	program, err := newProgram(vertexShaderSource, fragmentShaderSource)
+	if err != nil {
+		panic(err)
+	}
+	gl.UseProgram(program)
 
 	for !window.ShouldClose() {
 		// input
@@ -53,8 +159,12 @@ func main() {
 		// render
 		gl.ClearColor(0.2, 0.3, 0.3, 1)
 		gl.Clear(gl.COLOR_BUFFER_BIT)
+		gl.DrawArrays(gl.TRIANGLES, 0, 3)
 
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
+	gl.DeleteVertexArrays(1, &vao)
+	gl.DeleteBuffers(1, &vbo)
+	gl.DeleteProgram(program)
 }
