b274743de5150d60cb4bf2380e62d60a4f21e121
Matano <mwakimamatano@gmail.com>
2024-01-23 20:24:22 +0300

    out

--- cmd/main.go
+++ cmd/main.go
@@ -144,9 +144,11 @@ func main() {
   #version 330 core
 
   layout (location = 0) in vec3 aPos;
+  out vec4 vertexColor;
 
   void main() {
     gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); 
+    vertexColor = vec4(0.5, 0.0, 0.0, 1.0);
   }
 ` + shaderEndChar
 	compileShader(vertexShaderSource, gl.VERTEX_SHADER)
@@ -156,8 +158,9 @@ func main() {
 	fragmentShaderSource := `
   #version 330 core
   out vec4 FragColor;
+  in vec4 vertexColor;
   void main() {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    FragColor = vertexColor;
   }
 ` + shaderEndChar
 
